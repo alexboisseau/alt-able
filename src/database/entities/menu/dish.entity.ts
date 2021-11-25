953830bdafd55971e02d55ddd8f1063fa9e4c619
maximelarrieu <maxime.larrieu@ynov.com>
2021-11-25 14:36:55 +0100

    :sparkles: create dish and menu entities

---
+++ src/database/entities/menu/dish.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+
+export enum DishType {
+    APERITIF = "Apéritif",
+    STARTER = "Entrée",
+    MAIN = "Plat principal",
+    DESSERT = "DESSERT",
+    DRINK = "Boisson"
+}
+
+@Entity()
+export class Dish {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column('text')
+    description: string;
+
+    @Column({
+        type: 'enum',
+        enum: DishType,
+    })
+    type: DishType
+
+    @Column('decimal', { precision: 5, scale: 2})
+    price: number
+
+    @Column()
+    quantity: number
+
+}
---
+++ src/database/entities/menu/menu.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+import { Dish } from './dish.entity';
+
+@Entity()
+export class Menu {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column('text')
+    description: string;
+
+    @Column('decimal', { precision: 5, scale: 2})
+    price: number
+
+    @ManyToMany(() => Dish)
+    @JoinTable()
+    dishes: Dish[];
+}
