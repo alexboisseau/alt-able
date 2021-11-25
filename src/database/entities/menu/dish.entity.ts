d4d0d2881c8438a3a06f19f359ce803063714ef7
Thomas Le Naour <lenaourthomas@gmail.com>
2021-11-25 14:39:03 +0100

    chore(entities): format files

--- src/database/entities/menu/dish.entity.ts
+++ src/database/entities/menu/dish.entity.ts
@@ -1,34 +1,39 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
 
 export enum DishType {
-    APERITIF = "Apéritif",
-    STARTER = "Entrée",
-    MAIN = "Plat principal",
-    DESSERT = "DESSERT",
-    DRINK = "Boisson"
+  APERITIF = 'Apéritif',
+  STARTER = 'Entrée',
+  MAIN = 'Plat principal',
+  DESSERT = 'DESSERT',
+  DRINK = 'Boisson',
 }
 
 @Entity()
 export class Dish {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column('text')
-    description: string;
+  @Column('text')
+  description: string;
 
-    @Column({
-        type: 'enum',
-        enum: DishType,
-    })
-    type: DishType
+  @Column({
+    type: 'enum',
+    enum: DishType,
+  })
+  type: DishType;
 
-    @Column('decimal', { precision: 5, scale: 2})
-    price: number
+  @Column('decimal', { precision: 5, scale: 2 })
+  price: number;
 
-    @Column()
-    quantity: number
-
+  @Column()
+  quantity: number;
 }
--- src/database/entities/menu/menu.entity.ts
+++ src/database/entities/menu/menu.entity.ts
@@ -1,21 +1,27 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
 import { Dish } from './dish.entity';
 
 @Entity()
 export class Menu {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column('text')
-    description: string;
+  @Column('text')
+  description: string;
 
-    @Column('decimal', { precision: 5, scale: 2})
-    price: number
+  @Column('decimal', { precision: 5, scale: 2 })
+  price: number;
 
-    @ManyToMany(() => Dish)
-    @JoinTable()
-    dishes: Dish[];
+  @ManyToMany(() => Dish)
+  @JoinTable()
+  dishes: Dish[];
 }
