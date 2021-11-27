cf7d002fa2099b2af87f9b783a4ebfffe8d11b84
Alex Boisseau <alex.boisseau79@gmail.com>
2021-11-27 12:08:36 +0100

    Add validations for MenuDto

--- src/dtos/restaurant-menu/menu.ts
+++ src/dtos/restaurant-menu/menu.ts
@@ -1,14 +1,20 @@
 import { Type } from 'class-transformer';
+import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Id } from '..';
 import { DishDto } from './dish';
 
 export class CreateMenuDto {
+  @IsString()
+  @IsNotEmpty()
   public name!: string;
 
+  @IsString()
   public description!: string;
 
+  @IsNumber()
   public price!: number;
 
+  @IsArray()
   @Type(() => Id)
   public dishes!: Id[];
 }
